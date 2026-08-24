2e1b14ba489a3946fa4d9247bd6367083a599378
chriswilson-dev <chriswilson2004.work@gmail.com>
2026-08-25 01:51:46 +0530

    question for loops

---
+++ Loops/for_loops_even.py
@@ -0,0 +1,14 @@
+# n=int(input("Enter n: "))
+# for i in range (1,n+1):
+#     if i%2==0:
+#         print(i)
+
+# n=int(input("Enter n: "))
+# for i in range (1,11,+1):
+#     print(i*n)
+
+n=int(input("Enter n:"))
+fact=1
+for i in range(1,n+1,+1):
+    fact=fact*i
+print(fact)
