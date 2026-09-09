c4f72d548037b6082bfd98e9677bb59d9de3b79d
chriswilson-dev <chriswilson2004.work@gmail.com>
2026-09-10 02:24:51 +0530

    spaces and stars

---
+++ Loops/even_stars.py
@@ -0,0 +1,17 @@
+n=int(input("Enter n: "))
+# for i in range(1,n+1):
+#     for j in range(i*2):
+#         print("*",end="")
+#     print()
+
+# for i in range(n):
+#     for j in range(i*2+1):
+#         print("*",end="")
+#     print()
+
+for i in range(n):
+    for j in range(1,n-i):
+        print(" ",end="")
+    for k in range(i*2+1):
+        print("*",end="")
+    print()
