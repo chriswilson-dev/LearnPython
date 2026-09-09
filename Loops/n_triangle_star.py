ee7d1236cfa2f844615244f2308352077007d9cf
chriswilson-dev <chriswilson2004.work@gmail.com>
2026-09-10 01:27:06 +0530

    Star patterns speedrun

---
+++ Loops/n_triangle_star.py
@@ -0,0 +1,13 @@
+# n=int(input("Enter n: "))
+# for i in range (1,n+1):
+#     for j in range(1,i+1):
+#         print("*",end=" ")
+#     print()
+
+n=int(input("Enter n: "))
+for i in range(1,n+1):
+    for j in range(n-i):
+        print(" ",end="")
+    for k in range(i):
+        print("*",end="")
+    print()
