f6f18800a7dee622b8e466d49b132fc5b8c07fb0
chriswilson-dev <chriswilson2004.work@gmail.com>
2026-09-12 18:35:04 +0530

    number patterns continued

--- Loops/alternate_space_star.py
+++ Loops/alternate_space_star.py
@@ -1,7 +1,16 @@
 n=int(input("Enter number of rows: "))
+# for i in range(1,n+1):
+#     for j in range(n-i):
+#         print(" ",end="")
+#     for k in range(i):
+#         print(" *",end="")
+#     print()
+
+    #number question
+        # for i in range (1,n+1):
+        #     for j in range(1,i+1):
+        #         print(i,end="")
+        #     print()
+    #another approach
 for i in range(1,n+1):
-    for j in range(n-i):
-        print(" ",end="")
-    for k in range(i):
-        print(" *",end="")
-    print()
+    print(str(i)*i)
